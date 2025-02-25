fc34f020f621d4a7cdfad4bbbe9b7d7ba41aa14b
Rahuldev-5 <abhay.aslaliya@dev.appstonelab.com>
2025-02-25 15:57:28 +0530

    Add hexagon letter display and progress bar to Spelling Bee game screen

--- lib/screens/spelling_bee/controller/spelling_bee_controller.dart
+++ lib/screens/spelling_bee/controller/spelling_bee_controller.dart
@@ -4,6 +4,9 @@ class SpellingBeeController extends GetxController {
   String updateHint = "update_hint";
   String updateLatter = "update_Latter";
 
+  List<String> letterList = ['A', 'P', 'N', 'E', 'Y', 'M'];
+  String centerLetter = "T";
+
   void updateHintCountBuilder() {
     update([updateHint]);
   }
--- lib/screens/spelling_bee/spelling_bee_game_screen.dart
+++ lib/screens/spelling_bee/spelling_bee_game_screen.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_polygon/flutter_polygon.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gap/gap.dart';
 import 'package:get/get.dart';
+import 'package:wordles_multi_game_flutter/constants/constant_font.dart';
 
+import 'package:wordles_multi_game_flutter/constants/constant_image.dart';
+import 'package:wordles_multi_game_flutter/widgets/cust_inkwell.dart';
+import 'package:wordles_multi_game_flutter/widgets/rich_text.dart';
+
+import '../../widgets/custom_text.dart';
 import '/constants/constant_color.dart';
 import '/constants/constant_string.dart';
 import '/screens/spelling_bee/controller/spelling_bee_controller.dart';
@@ -11,24 +18,31 @@ import '/widgets/common_widget.dart';
 import '/widgets/pin_widget.dart';
 import '../../utils/shared_preference.dart';
 import '../../widgets/hint_button.dart';
-import '../wordles/controller/wordles_controller.dart';
+
 import '../wordles/guide_screen.dart';
 
-class SpellingBeeScreen extends StatelessWidget {
-  SpellingBeeScreen({super.key});
+class SpellingBeeScreen extends StatefulWidget {
+  const SpellingBeeScreen({super.key});
+
+  @override
+  State<SpellingBeeScreen> createState() => _SpellingBeeScreenState();
+}
+
+class _SpellingBeeScreenState extends State<SpellingBeeScreen> {
   final SpellingBeeController spellingBeeController =
       Get.find<SpellingBeeController>();
+  RxString word = "".obs;
 
-  final List<String> wordList = [
-    "Bee",
-    "Honey",
-    "Bee",
-    "Honey",
-    "Bee",
-    "Honey",
-    "Bee",
-    "Honey",
-  ];
+  RxList<String> wordList = <String>[].obs;
+
+  int totalSteps = 8;
+  int activeStep = 5;
+  double totalWidth = Get.width - 100.w;
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -69,15 +83,188 @@ class SpellingBeeScreen extends StatelessWidget {
         ],
         child: Column(
           children: [
+            _buildProgressBar(context),
             _buildPinWidget(),
             Container(
               height: 1,
               width: double.infinity,
               color: Colors.white,
-            )
+            ),
+            Obx(() {
+              return CustomRichText(
+                title: word.value.replaceAll(spellingBeeController.centerLetter,
+                    "#${spellingBeeController.centerLetter}"),
+                fancyTextStyle: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w600,
+                  color: ConstantColor.ff8E51FF,
+                  fontFamily: ConstantFont.moonchild,
+                ),
+                normalTextStyle: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w600,
+                  fontFamily: ConstantFont.moonchild,
+                ),
+              );
+            }),
+            SizedBox(
+              width: 300.h,
+              height: 300.h,
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  buildHexagon(spellingBeeController.letterList[0], -40, -70),
+                  buildHexagon(spellingBeeController.letterList[1], 40, -70),
+                  buildHexagon(spellingBeeController.letterList[2], -80, 0),
+                  buildHexagon(spellingBeeController.letterList[3], 80, 0),
+                  buildHexagon(spellingBeeController.letterList[4], -40, 70),
+                  buildHexagon(spellingBeeController.letterList[5], 40, 70),
+                  buildHexagon(spellingBeeController.centerLetter, 0, 0,
+                      isCenter: true),
+                ],
+              ),
+            ),
+            _buildDeleteRefreshAndEnterButton()
           ],
         ),
       ),
+    );
+  }
+
+  String addHashAboveChar(String text, String targetChar) {
+    int index = text.indexOf(targetChar);
+    if (index == -1) return text; // If targetChar is not found
+
+    // Split the string and insert '#'
+    String before = text.substring(0, index);
+    String after = text.substring(index);
+
+    return "$before\n#\n$after";
+  }
+
+  Widget _buildDeleteRefreshAndEnterButton() {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CommonButton(
+          onTap: () {
+            if (word.value.isNotEmpty) {
+              word.value = word.value.substring(0, word.value.length - 1);
+            }
+          },
+          title: "Delete",
+          radius: 40,
+        ),
+        Gap(10.w),
+        CommonIconButton(
+          bgColor: ConstantColor.ffffffff,
+          image: ConstantImage.refresh,
+          onTap: () {},
+        ),
+        Gap(10.w),
+        CommonButton(
+          onTap: () {
+            if (word.isNotEmpty) {
+              wordList.add(word.value);
+              word.value = "";
+            }
+          },
+          title: "Enter",
+          radius: 40,
+        ),
+      ],
+    );
+  }
+
+  Widget buildHexagon(String letter, double offsetX, double offsetY,
+      {bool isCenter = false}) {
+    return Positioned(
+      left: 120.w + offsetX,
+      top: 120.h + offsetY,
+      child: CustomInkWell(
+        onTap: () {
+          word.value += letter;
+        },
+        child: SizedBox(
+          width: 70.h,
+          height: 70.h,
+          child: ClipPolygon(
+            sides: 6,
+            borderRadius: 5.0,
+            rotate: 60.0,
+            boxShadows: [
+              PolygonBoxShadow(color: Colors.black, elevation: 1.0),
+              PolygonBoxShadow(color: Colors.grey, elevation: 5.0)
+            ],
+            child: Container(
+              decoration: BoxDecoration(
+                color:
+                    isCenter ? ConstantColor.ff8E51FF : ConstantColor.ffffffff,
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              alignment: Alignment.center,
+              child: CustomText(
+                txtTitle: letter,
+                style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                      color: isCenter ? Colors.white : Colors.black,
+                      fontSize: 30,
+                      fontWeight: FontWeight.w600,
+                      fontFamily: ConstantFont.moonchild,
+                    ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildProgressBar(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 20.h),
+      decoration: BoxDecoration(color: Colors.white),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          CustomText(
+            txtTitle: "Good",
+            style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                  fontWeight: FontWeight.w600,
+                ),
+          ),
+          Gap(8.w),
+          ...List.generate(
+            totalSteps,
+            (index) => buildStep(
+              isActive: index <= activeStep,
+              lineWidth: (totalWidth - (totalSteps * 10.w)) /
+                  (totalSteps - 1), // Dynamic line width
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildStep({required bool isActive, required double lineWidth}) {
+    return Row(
+      children: [
+        Container(
+          width: lineWidth,
+          height: 2,
+          color: isActive ? Colors.orange : Colors.grey[300],
+        ),
+        Container(
+          width: 10.w,
+          height: 10.w,
+          decoration: BoxDecoration(
+            color: isActive ? Colors.black : Colors.grey[400],
+            shape: BoxShape.circle,
+          ),
+        ),
+      ],
     );
   }
 
@@ -105,23 +292,29 @@ class SpellingBeeScreen extends StatelessWidget {
   }
 
   Widget _buildPinWidget() {
-    return Container(
-      height: 66.h,
-      padding: EdgeInsets.symmetric(
-        vertical: 14.h,
-      ),
-      child: ListView.separated(
-        scrollDirection: Axis.horizontal,
-        reverse: true,
-        shrinkWrap: true,
-        padding: EdgeInsets.symmetric(horizontal: 16.w),
-        itemCount: wordList.length,
-        itemBuilder: (context, index) => PinWidget(
-          title: wordList[index],
-          color: Colors.white.withAlpha(51),
+    return Obx(() {
+      return Container(
+        height: 66.h,
+        width: Get.width,
+        decoration: BoxDecoration(
+          color: Colors.white.withValues(alpha: 0.5),
         ),
-        separatorBuilder: (context, index) => Gap(16.w),
-      ),
-    );
+        padding: EdgeInsets.symmetric(
+          vertical: 14.h,
+        ),
+        child: ListView.separated(
+          scrollDirection: Axis.horizontal,
+          // reverse: true,
+          shrinkWrap: true,
+          padding: EdgeInsets.symmetric(horizontal: 16.w),
+          itemCount: wordList.length,
+          itemBuilder: (context, index) => PinWidget(
+            title: wordList[index],
+            color: Colors.white.withValues(alpha: 0.5),
+          ),
+          separatorBuilder: (context, index) => Gap(10.w),
+        ),
+      );
+    });
   }
 }
